c21eabd0bc82f3f9810004070324703f8288f26d
Dant777 <kolbt87@gmail.com>
2019-09-15 23:15:01 +0300

    Add project files.

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return Content("Hi world controll");
+            //return View();
+        }
+
+       
+    }
+}
