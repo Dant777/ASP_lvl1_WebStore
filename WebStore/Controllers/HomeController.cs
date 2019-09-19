9c5b06d91e3c3fa02f83c95e7507d5205b66efcb
Dant777 <kolbt87@gmail.com>
2019-09-19 13:12:22 +0300

    Create view BlogSingle

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -21,5 +21,9 @@ namespace WebStore.Controllers
         {
             return View();
         }
+        public IActionResult BlogSingle()
+        {
+            return View();
+        }
     }
 }
