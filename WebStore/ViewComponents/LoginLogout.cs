5f85eb9dae8560373cf0aeba599326e7e4d58a7f
Dant777 <kolbt87@gmail.com>
2019-10-03 14:03:46 +0300

    End lesson 6

---
+++ WebStore/ViewComponents/LoginLogout.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.ViewComponents
+{
+    public class LoginLogout : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+
+    }
+}
